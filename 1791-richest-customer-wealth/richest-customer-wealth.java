a76ae2c211f7fde55b9d286e0e5fac1278c1391c
Soumya-55 <sg7906032@gmail.com>
2025-12-22 10:14:03 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (11.03%) - LeetSync

---
+++ 1791-richest-customer-wealth/richest-customer-wealth.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max=0;
+        int n=accounts[0].length;
+        for(int i=0;i<accounts.length;i++){
+            int sum=0;
+            for(int j=0;j<n;j++){
+                sum +=accounts[i][j];
+            }
+            max=Math.max(sum,max);
+        }
+        return max;
+    }
+}
