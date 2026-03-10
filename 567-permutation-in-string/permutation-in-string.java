9962cefcf3ace6715b144902a51d5547de9c69a4
Soumya-55 <sg7906032@gmail.com>
2026-03-10 14:39:47 +0530

    Time: 7 ms (69.37%) | Memory: 44 MB (85.69%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length()>s2.length()) return false;
+        int[] freq1=new int[26];
+        int[] freq2=new int[26];
+        int k=s1.length();
+        for(int i=0;i<s1.length();i++) freq1[s1.charAt(i)-'a']++;
+
+        for(int i=0;i<s2.length();i++){
+            freq2[s2.charAt(i)-'a']++;
+            if(i>=k) freq2[s2.charAt(i-k)-'a']--;
+            if(Arrays.equals(freq1,freq2)) return true;
+        }
+        return false;
+        
+    }
+}
